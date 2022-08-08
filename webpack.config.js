9261db6fe776a4934b3038ce53a00b20f68f562d
SamLinux <oxedom@gmail.com>
2022-08-08 17:23:37 +0300

    demo2

--- webpack.config.js
+++ webpack.config.js
@@ -5,5 +5,12 @@ module.exports = {
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
+  },module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
   },
-};
+ };
